197889e8c8a2d6269a3e927a23a8340f0340e44d
Febry Muchammad <mc.ebryl@gmail.com>
2016-12-02 17:58:58 +0700

    paket

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl206162636/fastlaundry/Menu2.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl206162636/fastlaundry/Menu2.java
@@ -11,14 +11,19 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.Spinner;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class Menu2 extends Fragment implements View.OnClickListener{
 
+	Spinner paket;
+	ArrayList<String> pakets = new ArrayList<String>();
 	private Button buttonSave;
 	private FirebaseAuth firebaseAuth;
 	private DatabaseReference databaseReference;
