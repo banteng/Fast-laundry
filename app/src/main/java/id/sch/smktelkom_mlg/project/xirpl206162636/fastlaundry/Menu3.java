8b1ad7973beeb42a48f0dcc0a35d11e1282b7c56
SaskiaPuspita <dewsaski@gmail.com>
2016-12-03 21:44:55 +0700

    Menu3

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl206162636/fastlaundry/Menu3.java
@@ -0,0 +1,30 @@
+package id.sch.smktelkom_mlg.project.xirpl206162636.fastlaundry;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by toshiba on 03/12/2016.
+ */
+
+public class Menu3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.activity_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Menu 3");
+    }
+}
